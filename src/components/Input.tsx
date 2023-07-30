8b05b99cd6e7be6ee0ce8de13a72581b89397612
Nathan Wright <wright_nathan@hotmail.com>
2023-07-30 11:20:12 -0700

    refactor: move components to own modules

---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const Input = styled("input")({
+  width: "100%",
+  padding: "8px",
+  borderRadius: "4px",
+  border: "1px solid #ccc",
+});
+
+export default Input;
